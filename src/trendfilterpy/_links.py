ed7ec744f5c3b7f13af09085c97c6b1fa2d9432b
Erik Dains <eadains@gmail.com>
2025-01-20 23:10:31 -0500

    Adding power link function

--- src/trendfilterpy/_links.py
+++ src/trendfilterpy/_links.py
@@ -52,3 +52,21 @@ class LogitLink(LinkFunction):
             return cp.inv_pos(1 + cp.exp(-x))
         else:
             return 1 / (1 + np.exp(-x))
+
+
+class PowerLink(LinkFunction):
+    def __init__(self, p: float) -> None:
+        # TODO: Verify this is not zero
+        self.p = p
+
+    def eval(self, x: T) -> T:
+        if isinstance(x, cp.Expression):
+            return cp.power(x, self.p)
+        else:
+            return np.power(x, self.p)
+
+    def eval_inverse(self, x: T) -> T:
+        if isinstance(x, cp.Expression):
+            return cp.power(x, 1 / self.p)
+        else:
+            return np.power(x, 1 / self.p)
